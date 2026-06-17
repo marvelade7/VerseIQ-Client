5289835d627d6e8da759399cee150cc62e4895b8
Adewuyi Marvellous <marveladeadewuyi@gmail.com>
2026-06-17 16:27:12 +0100

    fix the links in the home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
     return (
         <div className="lg:py-20 md:py-15 py-12 lg:px-30 md:px-20 px-5">
             <h2 className="text-center font-semibold text-[#7C3AED] mt-5 lg:text-5xl md:text-4xl text-3xl">How well do you know the Bible?</h2>
             <p className="md:w-[80%] w-full text-center md:text-xl text-base sm:text-lg my-6 sm:my-8 mx-auto">Engage deeply with Scripture through our interactive quizzes. Test your knowledge, track your progress, and learn as you play in a focused, premium environment.</p>
             <div className="flex flex-col sm:flex-row items-center justify-center gap-3 sm:gap-5 mt-8 sm:mt-10">
-                <button className="w-full sm:w-auto bg-[#7C3AED] text-white text-base sm:text-lg px-4 py-2 rounded-md hover:bg-[#5a28c4] font-medium cursor-pointer duration-300">Start Quiz</button>
-                <button className="w-full sm:w-auto bg-white text-[#7C3AED] text-base sm:text-lg px-4 py-2 rounded-md border border-[#7C3AED] hover:bg-[#e9e3f4] cursor-pointer font-medium duration-300">Register</button>
+                <Link to='signup'><button className="w-full sm:w-auto bg-[#7C3AED] text-white text-base sm:text-lg px-4 py-2 rounded-md hover:bg-[#5a28c4] font-medium cursor-pointer duration-300">Start Quiz</button></Link>
+                <Link to='signup' ><button className="w-full sm:w-auto bg-white text-[#7C3AED] text-base sm:text-lg px-4 py-2 rounded-md border border-[#7C3AED] hover:bg-[#e9e3f4] cursor-pointer font-medium duration-300">Register</button></Link>
             </div>
         </div>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div className="sticky top-0 z-10 flex items-center justify-between gap-3 lg:px-20 sm:px-10 px-4 py-2 shadow-md bg-white text-black">
@@ -8,8 +10,8 @@ const Navbar = () => {
                 <li className="cursor-pointer hover:text-[#7C3AED] duration-300 font-medium">Profile</li>
             </ul>
             <div className="flex items-center md:gap-5 gap-2 shrink-0">
-                <button className="bg-white text-[#7C3AED] sm:px-4 px-3 sm:py-2 py-1.5 rounded-md border border-[#7C3AED] hover:bg-[#e9e3f4] cursor-pointer text-sm sm:text-base font-medium duration-300">Sign In</button>
-                <button className="bg-[#7C3AED] hidden md:block text-white px-4 py-2 rounded-md hover:bg-[#5a28c4] font-medium cursor-pointer duration-300">Start Quiz</button>
+                <Link to='signin'><button className="bg-white text-[#7C3AED] sm:px-4 px-3 sm:py-2 py-1.5 rounded-md border border-[#7C3AED] hover:bg-[#e9e3f4] cursor-pointer text-sm sm:text-base font-medium duration-300">Sign In</button></Link>
+                <Link to='/signup'><button className="bg-[#7C3AED] hidden md:block text-white px-4 py-2 rounded-md hover:bg-[#5a28c4] font-medium cursor-pointer duration-300">Start Quiz</button></Link>
                 <i className="bi bi-list md:hidden text-2xl cursor-pointer text-[#7C3AED]"></i>
             </div>
         </div>
